16eb40137317be93c962736f13110da9edbf53c4
Raphael Ivan <raphaivan@gmail.com>
2020-04-15 00:48:33 -0300

    Change config.ru configuration to nginx

--- config.ru
+++ config.ru
@@ -1,5 +1,5 @@
+require 'rubygems'
 require "sinatra"
 
-require File.expand_path '../main.rb', __FILE__
-
-run Site
+require './main.rb'
+run Sinatra::Application
--- main.rb
+++ main.rb
@@ -1,17 +1,14 @@
 require "sinatra"
 require "sinatra/base"
 
-class Site < Sinatra::Base
+get "/" do
+  erb :index
+end
 
-  get "/" do
-    erb :index
-  end
+get "/about" do
+  erb :about
+end
 
-  get "/about" do
-    erb :about
-  end
-
-  get "/contact" do
-    erb :contact
-  end
+get "/contact" do
+  erb :contact
 end
